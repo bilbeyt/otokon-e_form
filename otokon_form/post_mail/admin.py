62f95d6bd83703dbec99a991c29d70bbbccf50ee
Tolga <bilbeyt@gmail.com>
2016-07-26 15:11:01 +0300

    favicon added

--- otokon_form/post_mail/admin.py
+++ otokon_form/post_mail/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from post_mail.models import EmailMessage
+from django.core.mail import send_mail
+from form.models import Form
 
 
 class EmailMessageAdmin(admin.ModelAdmin):
@@ -7,10 +9,19 @@ class EmailMessageAdmin(admin.ModelAdmin):
     list_filter = ["time"]
     search_fields = ["sender", "title"]
     exclude = ["sender"]
-   
+
     def save_model(self, request, obj, form, change):
-	obj.sender = request.user
-	super(EmailMessageAdmin, self).save_model(request, obj, form, change)
+        obj.sender = request.user
+        ids = request.POST.getlist("to")
+        mails = Form.objects.filter(id__in=ids).values_list("mail", flat=True)
+        super(EmailMessageAdmin, self).save_model(request, obj, form, change)
+        send_mail(
+            obj.title,
+            obj.content,
+            "otokon@itu.edu.tr",
+            mails,
+            fail_silently=False
+    	)
 
 
 admin.site.register(EmailMessage, EmailMessageAdmin)
--- otokon_form/post_mail/models.py
+++ otokon_form/post_mail/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from form.models import Form
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.core.mail import send_mail
 from django.contrib.auth.models import User
 
 
@@ -15,14 +12,3 @@ class EmailMessage(models.Model):
 
     def __str__(self):
         return self.title
-
-
-@receiver(post_save, sender=EmailMessage, dispatch_uid="emailmessage_identifier")
-def EmailMessage_sender(sender, instance, *args, **kwargs):
-    send_mail(
-	    instance.title,
-	    instance.content,
-	    "otokon@itu.edu.tr",
-        instance.to.all().values_list("mail", flat=True),
-	    fail_silently=False,
-    )
